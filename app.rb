1018633a7bbc77a060f3f85e683489b3e1deaa67
Chris Junker <elevatemedfit@gmail.com>
2019-09-03 17:04:09 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,25 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-end
+
+    get '/name' do
+      "My name is "
+    end
+
+    get '/hometown' do
+      "My hometown is "
+    end
+
+    get '/favorite-song' do
+      "My favorite song is "
+    end
+
+  end
+
+  # medicines_controller.rb
+
+  # get '/medicines' do
+  #   @medicines = Medicine.all
+  #
+  #   erb :'medicines/index.html.erb'
+  # end

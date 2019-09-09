15416e2605a2682a09d4cb13fdb2637fa5218bef
Jack Atherton <jackrrye@gmail.com>
2019-09-09 00:24:50 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,22 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/' do
+        "Hello, World!"
+    end
+
+    get 'name' do
+        @name = "My name is Jack."
+        erb :'name.html.erb'
+    end
+
+    get 'hometown' do
+        "My hometown is Houston."
+    end
+
+    get 'favorite-song' do
+        "My favorite song is Trapeze Swinger."
+    end
+
 end
